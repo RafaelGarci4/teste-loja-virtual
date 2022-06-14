ea5d320bdaa4656d4ec1418f6582bd755c32320b
RafaelGarci4 <rafa.ferreira888@gmail.com>
2022-06-14 17:39:27 -0300

    final

--- src/main/java/org/example/TesteInsercaoComParametro.java
+++ src/main/java/org/example/TesteInsercaoComParametro.java
@@ -9,20 +9,35 @@ public class TesteInsercaoComParametro {
         ConnectionFactory factory = new ConnectionFactory();
 
         Connection connection = factory.recuperarConexao();
-
+        connection.setAutoCommit(false);
        // String sql = ;
-        PreparedStatement stm =  connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)",Statement.RETURN_GENERATED_KEYS);
-        adicionarVariavel("SmartTV", "45 Polegadas", stm);
-        adicionarVariavel("Radio", "Radio de bateria", stm);
+
+        try{
+            PreparedStatement stm =  connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)",Statement.RETURN_GENERATED_KEYS);
+            adicionarVariavel("SmartTV", "45 Polegadas", stm);
+            adicionarVariavel("Radio", "Radio de bateria", stm);
+
+            connection.commit();
+            stm.close();
+            connection.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("ROLLBACK EXECUTADO");
+            connection.rollback();
+        }
+
     }
 
     private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
         stm.setString(1, nome);
         stm.setString(2, descricao);
+
         //RETORNA  A CHAVE GERADA
         stm.execute();
 
         ResultSet rst = stm.getGeneratedKeys();
+
         while (rst.next()){
             Integer id = rst.getInt(1);
             System.out.println("o id criado foi: "+id);
